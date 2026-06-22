71d12e15574852c45611a7d09a25ddd8d0b43ebc
MARIUM BINTE MUHAMMAD <165379776+marium0908@users.noreply.github.com>
2026-06-22 18:23:49 +0600

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { createAuthClient } from "better-auth/react";
+import { createAuthClient } from "better-auth/client";
 
 export const authClient = createAuthClient({
   baseURL: window.location.origin || "http://localhost:3000"
